73e76354bfce2bbb8e5fbc3383422d86ecb7f257
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-02-03 23:33:39 +0900

    エディタの中央部分のエディタたらしめるところをコンポーネントにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { HStack, Stack, Theme } from '@chakra-ui/react';
+import { Bleed, Flex, HStack, Theme } from '@chakra-ui/react';
 import HeaderController from './HeaderController';
+import SequentialEditor from './SequentialEditor';
 
 function App() {
   return (<>
     <Theme appearance='dark'>
-      <Stack h={"100vh"} >
+      <Flex h={"100vh"} w={"100vw"} direction={"column"} >
         <HStack w={"100vw"}>
           <HeaderController />
         </HStack>
-      </Stack>
+        <Bleed flex={1} bgColor={"gray.900"}>
+          <SequentialEditor />
+        </Bleed>
+      </Flex>
     </Theme>
   </>);
 }
---
+++ src/SequentialEditor.tsx
@@ -0,0 +1,10 @@
+import { HStack, Stack } from "@chakra-ui/react";
+
+export default function SequentialEditor() {
+  return (
+    <Stack w={"100vw"} h={"100%"} gap={0} >
+      <HStack w={"100vw"} h={"100%"} >
+      </HStack>
+    </Stack>
+  );
+}
