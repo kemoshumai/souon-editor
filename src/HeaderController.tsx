938c86d08c9870878fa5b644e6e46f04a3b9cffd
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-02-04 16:57:27 +0900

    ヘッダーにプロジェクト名の入力フィールドを追加し、譜面がない場合のメッセージを表示する機能を実装

--- src/HeaderController.tsx
+++ src/HeaderController.tsx
@@ -1,11 +1,25 @@
-import { Button, HStack, Separator, Stack } from "@chakra-ui/react";
+import { Button, HStack, Input, Separator, Stack } from "@chakra-ui/react";
 import { PiPlay } from "react-icons/pi";
+import { useSnapshot } from "valtio";
+import store from "./store/store";
+import { useState } from "react";
 
 export default function HeaderController() {
+
+  const snap = useSnapshot(store);
+  
+  const [inputName, setInputName] = useState(snap.project.name);
+
   return (
     <Stack w={"100vw"} gap={0}>
-      <HStack w={"100vw"} >
-        <Button w={"10"} h={"10"}><PiPlay /></Button>
+      <HStack w={"100vw"}>
+        <Button w="10" h="10"><PiPlay /></Button>
+        <Input
+          w={300}
+          value={inputName}
+          onChange={e => setInputName(e.target.value)}
+          onBlur={() => store.project.name = inputName}
+        />
       </HStack>
       <Separator />
     </Stack>
--- src/SequentialEditor.tsx
+++ src/SequentialEditor.tsx
@@ -1,4 +1,4 @@
-import { HStack, Stack } from "@chakra-ui/react";
+import { Center, HStack, Show, Stack, Text } from "@chakra-ui/react";
 import { useSnapshot } from "valtio";
 import store from "./store/store";
 
@@ -9,6 +9,13 @@ export default function SequentialEditor() {
   return (
     <Stack w={"100vw"} h={"100%"} gap={0} >
       <HStack w={"100vw"} h={"100%"} >
+        <Show when={snap.project.charts.length == 0} >
+          <Center w={"100vw"} h={"100%"} >
+            <Stack>
+              <Center><Text fontSize={"xl"} color={"gray.400"} >表示する譜面がありません</Text></Center>
+            </Stack>
+          </Center>
+        </Show>
       </HStack>
     </Stack>
   );
