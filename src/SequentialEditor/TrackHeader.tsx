e8d43bd295e7ce70b3f1b75e1fe0e66b6a87715d
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-02-05 12:25:46 +0900

    トラックのヘッダーのzIndexを調整

--- src/SequentialEditor/TrackHeader.tsx
+++ src/SequentialEditor/TrackHeader.tsx
@@ -1,7 +1,7 @@
 import { Bleed } from "@chakra-ui/react";
 
 export default function TrackHeader(props: { children: React.ReactNode; }) {
-  return (<Bleed position={"sticky"} w={"100%"} h={100} borderBottom={"solid 1px"} borderColor={"black"} top={0} >
+  return (<Bleed position={"sticky"} w={"100%"} h={100} borderBottom={"solid 1px"} borderColor={"black"} top={0} zIndex={2} bgColor={"gray.800"}>
     {props.children}
   </Bleed>);
 }
