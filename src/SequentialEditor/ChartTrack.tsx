a42c9301292b87210b0fee0c6a45ab45e0ac7f7d
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-02-10 11:42:44 +0900

    小節線を表示する親要素だけ作った。マコモ進捗。

--- src/SequentialEditor/ChartTrack.tsx
+++ src/SequentialEditor/ChartTrack.tsx
@@ -1,4 +1,4 @@
-import { Bleed, Center, Editable, Flex, Stack, Text } from "@chakra-ui/react";
+import { Bleed, Center, Editable, Flex, For, Stack, Text } from "@chakra-ui/react";
 import { useSnapshot } from "valtio";
 import store from "../store/store";
 import { useState } from "react";
@@ -49,12 +49,23 @@ export default function ChartTrack(props: { uuid: string; }) {
 
   return (
     <Track uuid={props.uuid} header={header} w={350 / 12 * ( chart?.laneNumber ?? 1 )} >
-      <Flex position={"relative"} top={0} h={"100%"} w={"100%"}>
-        <Bleed borderLeft={"solid 1px"} borderRight={"solid 1px"} flex={1} bgColor={"red.800"}></Bleed>
-        {[...Array(chart?.laneNumber ?? 0)].map((_, i) => <Bleed key={i} borderLeft={"solid 1px"} borderRight={"solid 1px"} flex={1} 
-          bgColor={["gray.800", "gray.700"][pattern[i%24]]}
-        ></Bleed>)}
-      </Flex>
+      <Bleed position={"relative"} w={"100%"} h={"100%"} >
+        <Flex position={"absolute"} top={0} h={"100%"} w={"100%"}>
+          <Bleed borderLeft={"solid 1px"} borderRight={"solid 1px"} flex={1} bgColor={"red.800"}></Bleed>
+          {[...Array(chart?.laneNumber ?? 0)].map((_, i) => <Bleed key={i} borderLeft={"solid 1px"} borderRight={"solid 1px"} flex={1} 
+            bgColor={["gray.800", "gray.700"][pattern[i%24]]}
+          ></Bleed>)}
+        </Flex>
+        <Bleed position={"absolute"} left={0} bottom={0} w={"100%"} h={"100%"} >
+          <For each={snap.project.musicTempoList} fallback={<></>} >
+            {(tempo, _) => (
+              <Bleed key={tempo.uuid} w={"100%"} position={"absolute"} left={0} bottom={snap.project.getYPosition(tempo.position)} >
+                {/* ここの計算もっと練らないとな */}
+              </Bleed>
+            )}
+          </For>
+        </Bleed>
+      </Bleed>
     </Track>
   );
 }
