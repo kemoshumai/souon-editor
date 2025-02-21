1a45bb574a530012af79f3590671670aa6cbd678
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-02-21 17:53:30 +0900

    小節数をGUIから設定できるようにした

--- src/SequentialEditor/TempoTrack/TempoMarker.tsx
+++ src/SequentialEditor/TempoTrack/TempoMarker.tsx
@@ -1,4 +1,4 @@
-import { Bleed, Center, Editable, HStack, Text } from "@chakra-ui/react";
+import { Bleed, Center, Editable, HStack, Stack, Text } from "@chakra-ui/react";
 import { useSnapshot } from "valtio";
 import store from "../../store/store";
 import { useEffect, useState } from "react";
@@ -11,6 +11,7 @@ export default function TempoMarker( props: { uuid: string} ) {
 
   const [bpm, setBpm] = useState(tempo.tempo);
   const [beat, setBeat] = useState(tempo.beat);
+  const [length, setLength] = useState(tempo.length);
 
   useEffect(() => {
     const tempo = store.project.musicTempoList.find(t => t.uuid === props.uuid)!;
@@ -21,6 +22,11 @@ export default function TempoMarker( props: { uuid: string} ) {
     const tempo = store.project.musicTempoList.find(t => t.uuid === props.uuid)!;
     tempo.tempo = bpm;
   }, [bpm]);
+
+  useEffect(() => {
+    const tempo = store.project.musicTempoList.find(t => t.uuid === props.uuid)!;
+    tempo.length = length;
+  }, [length]);
 
   const crop = (value: number, defaultValue: number) => {
     if (value < 1) return 1;
@@ -33,20 +39,31 @@ export default function TempoMarker( props: { uuid: string} ) {
 
   return (<>
     <Bleed borderBottom={"solid 1px white"} w={"100%"} h={50} position={"absolute"} left={0} bottom={snap.project.getYPosition(snap.project.getTemporalPositionFromTempoEvent(tempo))} >
-      <Center position={"absolute"} bottom={0} w={"100%"} >
-        <HStack>
-          <Editable.Root textAlign={"center"} defaultValue={bpm.toString()} w={7} onValueChange={e => setBpm(crop(parseInt(e.value), 120))} value={bpm.toString()}>
-            <Editable.Preview />
-            <Editable.Input />
-          </Editable.Root>
-          <Text color={"gray.400"}>BPM</Text>
-          <Editable.Root textAlign={"center"} defaultValue={beat.toString()} w={7} onValueChange={e => setBeat(crop(parseInt(e.value), 4))} value={beat.toString()}>
-            <Editable.Preview />
-            <Editable.Input />
-          </Editable.Root>
-          <Text color={"gray.400"}>拍</Text>
-        </HStack>
-      </Center>
+      <Stack position={"absolute"} bottom={0} w={"100%"} gap={0} >
+        <Center>
+          <HStack>
+            <Editable.Root textAlign={"center"} defaultValue={bpm.toString()} w={7} onValueChange={e => setBpm(crop(parseInt(e.value), 120))} value={bpm.toString()}>
+              <Editable.Preview />
+              <Editable.Input />
+            </Editable.Root>
+            <Text color={"gray.400"}>BPM</Text>
+            <Editable.Root textAlign={"center"} defaultValue={beat.toString()} w={7} onValueChange={e => setBeat(crop(parseInt(e.value), 4))} value={beat.toString()}>
+              <Editable.Preview />
+              <Editable.Input />
+            </Editable.Root>
+            <Text color={"gray.400"}>拍子</Text>
+          </HStack>
+        </Center>
+        <Center>
+          <HStack>
+            <Editable.Root textAlign={"center"} defaultValue={length.toString()} w={14} onValueChange={e => setLength(crop(parseInt(e.value), 999))} value={length.toString()}>
+              <Editable.Preview />
+              <Editable.Input />
+            </Editable.Root>
+            <Text color={"gray.400"}>小節</Text>
+          </HStack>
+        </Center>
+      </Stack>
     </Bleed>
   </>);
 }
