9c27bb99cb82bd73f761b164471e320eb0e1bfa0
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-03-03 22:53:48 +0900

    バナーに自分のロゴ付けた

--- src/HeaderController/TitleBanner.tsx
+++ src/HeaderController/TitleBanner.tsx
@@ -1,11 +1,11 @@
-import { Bleed, HStack, Separator, Text } from "@chakra-ui/react";
+import { Flex, HStack, Text } from "@chakra-ui/react";
+import kemoshumai from "./kemoshumai.svg";
 
 export default function TitleBanner() {
-  return (<Bleed position={"absolute"} top={0} right={0} w={"20vw"} h={"100%"}>
+  return (<Flex position={"absolute"} top={0} right={0} w={"100vw"} h={"100%"} pointerEvents={"none"} justify={"right"} pr={2} >
     <HStack>
       <Text fontStyle={"italic"} mt={2}>{"Souon Editor v0.00"}</Text>
-      <Separator orientation={"vertical"} />
-      <Text fontStyle={"italic"} mt={2}>{"CREATED BY Kemoshumai"}</Text>
+      <img src={kemoshumai} width={"100px"} color="white" style={{filter: "invert(71%) sepia(0%) saturate(711%) hue-rotate(143deg) brightness(88%) contrast(80%)"}} />
     </HStack>
-  </Bleed>)
+  </Flex>)
 }
