db24a8f5e6a49d555fa3193a2b7067d8c48deb7d
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-03-04 15:56:29 +0900

    スペースキーで再生できるようにした

--- src/AudioSystem.tsx
+++ src/AudioSystem.tsx
@@ -1,6 +1,8 @@
 import { useSnapshot } from "valtio";
 import store from "./store/store";
 import { useEffect, useRef } from "react";
+import { useInterval, useKeyPressEvent } from "react-use";
+import TemporalPosition from "./store/temporalPosition";
 
 export default function AudioSystem() {
 
@@ -28,6 +30,35 @@ export default function AudioSystem() {
     }
 
   }, [snap.project.music]);
+
+  useEffect(() => {
+
+    if(!audioRef.current) return;
+
+    console.log("audioRef.current.currentTime", audioRef.current.currentTime);
+
+    if(audioRef.current.paused) {
+      audioRef.current.currentTime = snap.project.playingPosition.seconds;
+    }
+
+  }, [snap.project.playingPosition]);
+
+  useKeyPressEvent(' ', () => {
+    if(!audioRef.current) return;
+    audioRef.current.currentTime = snap.project.playingPosition.seconds; 
+    if(audioRef.current.paused) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  });
+
+  const updatePlayingPosition = () => {
+    if(!audioRef.current) return;
+    store.project.playingPosition = TemporalPosition.createWithSeconds(audioRef.current.currentTime);
+  }
+
+  useInterval(updatePlayingPosition, 50);
 
   if (!snap.project.music) return <></>;
 
