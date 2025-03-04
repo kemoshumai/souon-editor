00dc264f381b46c71a62b8a5a79e067563eaa76d
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2025-03-04 16:00:20 +0900

    スペースキーでのスクロールを無効化

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import HeaderController from './HeaderController';
 import SequentialEditor from './SequentialEditor';
 import { Toaster } from './components/ui/toaster';
 import AudioSystem from './AudioSystem';
+import { useEffect } from 'react';
 
 function App() {
+
+  // スペースキーでのスクロールを無効化
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.code === "Space") {
+        e.preventDefault();
+      }
+    });
+
+    return () => {
+      window.removeEventListener("keydown", () => {});
+    }
+  });
+
   return (<>
     <Theme appearance='dark'>
       <Toaster />
